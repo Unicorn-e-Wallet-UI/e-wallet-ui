05bc25b3aadeb6226bfdba9f1278f554f5c893b7
Bolaji Disu <abolajiidiisu@gmail.com>
2023-01-29 01:49:21 +0100

    commemted out signuppage link, not yet implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import RegistrationPageOne from './component/pages/Registration-pages/verification/kycFormsPageOne/kycPageOne';
 import RegistrationPageTwo from './component/pages/Registration-pages/verification/kycFormsPageTwo/kycPageTwo';
 import LoginPage from './component/pages/Registration-pages/Log in/login';
-import SignUpPage from './component/pages/Registration-pages/signUp/signup';
+// import SignUpPage from './component/pages/Registration-pages/Sign up/signup';
 import ResetPassword from './component/pages/Registration-pages/ResetPasswoord/resetPassword';
 import EnterOTPPage from './component/pages/Registration-pages/OTP/otp';
 import EnterNewPassword from './component/pages/Registration-pages/ResetPasswoord/newPassword';
@@ -15,7 +15,7 @@ function App() {
         <Route exact path='/registration-page-one' element={<RegistrationPageOne />}  />
         <Route exact path='/registration-page-two' element={<RegistrationPageTwo />}  />
         <Route exact path='/login-page' element={<LoginPage/>} />
-        <Route exact path='/signup-page' element={<SignUpPage/>}/>
+        {/* <Route exact path='/signup-page' element={<SignUpPage/>}/> */}
         <Route exact path='/reset-password' element={<ResetPassword />}  />
         <Route exact path='/get-otp' element={<EnterOTPPage />}  />
         <Route exact path='/new-password' element={<EnterNewPassword />}  />
